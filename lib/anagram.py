02ee9d4fb3c053fe3e1f8daa573954e98eab1689
Esther Karanja <est.karanja@gmail.com>
2023-10-18 16:52:04 -0700

    last 2 tests failing

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, my_list):
+        value =[item for item in my_list]
+        if len(self.word) != len(value):
+            return []
+        else:
+            if sorted(self.word) == sorted(value):
+                if value == 1:
+                    return value
+                pass
+
+        
+        
